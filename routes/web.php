095a511f4671a482b482fe2f196e2d986136d2c0
Hiroki1238 <c14h14n3nao3s@gmail.com>
2022-06-18 13:37:57 +0900

    [add]データ取得件数制限機能の追加、ペジネーションの追加

--- routes/web.php
+++ routes/web.php
@@ -26,4 +26,4 @@ Route::get('/', function() {
 Route::get('/posts', 'PostController@index');
 */
 
-Route::get('/', 'PostController@index');
+Route::get('/', 'PostController@index'); //PostController.phpのindex()を呼び出してる
