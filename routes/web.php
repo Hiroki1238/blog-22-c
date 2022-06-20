f47a53d84aeaf00c129a9facb37f5d96ded13ca7
Hiroki1238 <c14h14n3nao3s@gmail.com>
2022-06-20 17:49:55 +0900

    [add]edit.blade.php(editボタン)の追加

--- routes/web.php
+++ routes/web.php
@@ -31,3 +31,6 @@ Route::get('/posts/create', 'PostController@create');
 Route::get('/posts/{post}', 'PostController@show');
 
 Route::post('/posts', 'PostController@store');
+
+Route::put('/posts/{post}', 'PostController@update');
+Route::get('/posts/{post}/edit', 'PostController@edit');
